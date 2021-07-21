5da8a1c2a96e458144537fa8434040f89caaeac4
sharankarthikyan <karthikeyansharan00@gmail.com>
2021-07-21 14:03:24 +0530

    large push

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -6,18 +6,18 @@ import { selectDirectorySections } from '../../redux/directory/directory.selecto
 
 import MenuItem from '../menu-item/menu-item.component';
 
-import { DirectoryMenuContainer } from './directory.styles';
+import './directory.styles.scss';
 
 const Directory = ({ sections }) => (
-  <DirectoryMenuContainer>
+  <div className='directory-menu'>
     {sections.map(({ id, ...otherSectionProps }) => (
       <MenuItem key={id} {...otherSectionProps} />
     ))}
-  </DirectoryMenuContainer>
+  </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  sections: selectDirectorySections,
+  sections: selectDirectorySections
 });
 
 export default connect(mapStateToProps)(Directory);
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -5,30 +5,24 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
-import {
-  CollectionPageContainer,
-  CollectionTitle,
-  CollectionItemsContainer,
-} from './collection.styles';
+import './collection.styles.scss';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
   return (
-    <CollectionPageContainer>
-      <CollectionTitle>{title}</CollectionTitle>
-      <CollectionItemsContainer>
-        {items.map((item) => (
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </CollectionItemsContainer>
-    </CollectionPageContainer>
+      </div>
+    </div>
   );
 };
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    collection: selectCollection(ownProps.match.params.collectionId)(state),
-  };
-};
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
 
 export default connect(mapStateToProps)(CollectionPage);
